0d3123a75a646a08a04c4bec1f2d82c834c5198b
Andres Bilevich <andresbilevich@gmail.com>
2025-11-14 14:31:40 +0100

    chore: added sample data and started work on user query

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,10 +1,50 @@
 import { buildSchema } from 'graphql';
 
-const schema = buildSchema(`type Query { hello: String } `);
+const schema = buildSchema(`
+  schema { 
+    query: Query
+    mutation: Mutation
+  }
+
+  type Query { 
+    hello: String
+    users: [User]
+  } 
+
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+  }
+  
+  input CreateUserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+  }
+
+  type Mutation { 
+    createUser(input: CreateUserInput): User
+  }
+  
+`);
 
 const root = {
   hello: () => {
     return 'Hello from GraphQL!';
+  },
+  users: () => {
+    return [
+      {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        email: '',
+      },
+    ];
   },
 };
 
